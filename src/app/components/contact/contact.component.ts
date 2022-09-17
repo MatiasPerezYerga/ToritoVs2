40c0d0913a96b0b5d5c477aefbd9e62eda1d5616
matiasyerga <matiasyerga@gmail.com>
2022-09-17 20:47:09 -0300

    contactTemplate

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Report} from '../../models/report';
+import{GiftcardService} from '../../services/giftcard.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+
+  public report: Report;
+  constructor(private _giftcardService: GiftcardService,) {
+
+
+  this.report= new Report('','','','',0);
+
+   }
 
   ngOnInit(): void {
   }
+
+
+  reportarProblema(form:any) {
+
+  
+console.log("Aquí se enviará");
+console.log(this.report);
+
+this._giftcardService.saveReport(this.report).subscribe(
+
+response=>{
+
+  if(response){
+
+    console.log("El siguiente reporte ha sido enviado.");
+    form.reset();
+
+  }
+},
+
+
+error =>{
+
+console.log(<any>error+"No hay respuesta del backend" )
+
+},
+
+
+  )
+
+
+
+
+}
+
 
 }
